b7d78f2ad058e8cb8eae820749d41d1400d20898
Aviral Vardiya <aviralvardiya10@gmail.com>
2024-04-09 18:04:32 +0530

    try catch block added in p2ptransaction action

--- apps/user-app/app/lib/actions/p2pTransfer.tsx
+++ apps/user-app/app/lib/actions/p2pTransfer.tsx
@@ -1,45 +1,63 @@
-"use server"
+"use server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth";
 import prisma from "@repo/db/client";
 
 export async function p2pTransfer(to: string, amount: number) {
-    const session = await getServerSession(authOptions);
-    const from = session?.user?.id;
-    if (!from) {
-        return {
-            message: "Error while sending"
-        }
-    }
-    const toUser = await prisma.user.findFirst({
+  const session = await getServerSession(authOptions);
+  const from = session?.user?.id;
+  if (!from) {
+    return {
+      message: "Error while sending",
+    };
+  }
+
+  try {
+    
+      const toUser = await prisma.user.findFirst({
         where: {
-            number: to
-        }
-    });
-
-    if (!toUser) {
+          number: to,
+        },
+      });
+    
+      if (!toUser) {
         return {
-            message: "User not found"
-        }
-    }
-     prisma.$transaction(async (tx) => {
+          message: "User not found",
+        };
+      }
+      prisma.$transaction(async (tx) => {
         await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
-
+    
         const fromBalance = await tx.balance.findUnique({
-            where: { userId: Number(from) },
-          });
-          if (!fromBalance || fromBalance.amount < amount) {
-            throw new Error('Insufficient funds');
-          }
-          await new Promise(r => setTimeout(r, 4000));
-          await tx.balance.update({
-            where: { userId: Number(from) },
-            data: { amount: { decrement: amount } },
-          });
+          where: { userId: Number(from) },
+        });
+        if (!fromBalance || fromBalance.amount < amount) {
+          throw new Error("Insufficient funds");
+        }
+        //   await new Promise(r => setTimeout(r, 4000));  //had added this to test two quick requests
+        await tx.balance.update({
+          where: { userId: Number(from) },
+          data: { amount: { decrement: amount } },
+        });
+    
+        await tx.balance.update({
+          where: { userId: toUser.id },
+          data: { amount: { increment: amount } },
+        });
+    
+        await tx.p2PTransaction.create({
+            data:{
+                amount:amount,
+                fromUserId:Number(from),
+                toUserId:toUser.id,
+                timeStamp: new Date()
+            }
+        })
+      });
+  } catch (error) {
+    console.log(error)
+    return {message:error.message}
+    
+  }
 
-          await tx.balance.update({
-            where: { userId: toUser.id },
-            data: { amount: { increment: amount } },
-          });
-    });
 }
